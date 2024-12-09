17059da8b4895413ccdc6854bd92f820b5683e8b
tahminatanjidamiti <mity2027@gmail.com>
2024-12-09 15:50:12 +0600

    delete method added for visa application collection

--- index.js
+++ index.js
@@ -103,7 +103,13 @@ async function run() {
         });
 
 
-
+        // Delete from my visa application
+        app.delete('/my_visa_applications/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await applicationCollection.deleteOne(query);
+            res.send(result);
+        });
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
